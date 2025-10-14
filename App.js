7aca4c26b6742b326826ba19bc5bc3121ab82125
Althea Carias <altheacarias37@gmail.com>
2025-10-14 21:14:36 +0800

    Update App.js

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+//import bag o
+import HardLevelFull from "./HardLevelFull";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Desie B. Torrenueva</Text>
+      <Text> Desie B. Torrenueva</Text>
       <StatusBar style="auto" />
     </View>
   );
